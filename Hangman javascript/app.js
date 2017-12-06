76f95ddf5f6943b73fb2743341a6f604aa706a9d
Melesa Martin <melesamartin@Melesa-Martins-MacBook-Pro.local>
2017-12-06 00:50:50 -0600

    added files and npm pkg

---
+++ Hangman javascript/app.js
@@ -0,0 +1,48 @@
+
+// start playing the game
+// initialize a var guessesLeft
+// get a random word from a list of words
+
+// save the word to an object
+// print the word (underscores only)
+// guess a letter
+// process the letter -> correct
+
+// new Word("abc")
+
+// function Word(word) {
+//     var allChars = word.split("")
+//         var letter = new Letter("a")
+//         letters.push(letter)
+//         "a", "b", "c"
+//     }
+// }
+
+
+// Word
+// create all the letters
+// guess method - check all letters (at least one)
+// print solution
+// print guessed progress with the blanks
+// check if youve solved the whole word
+
+
+
+
+// Letter
+// guess(char) - is this myself?
+// printMe (prints _ or the letter if already guessed)
+// printSolution - prints letter no matter if guessed
+
+
+
+
+
+
+
+
+
+
+// You have 10 guesses left
+// _ _ _
+// Guess a letter > a
